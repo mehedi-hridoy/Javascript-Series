e0f8634f8a46b79ed410ab843057847d792d02ea
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-12-13 14:29:26 +0000

    datatypes

---
+++ 01Basics/01dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //to use the newer version of javascript
+// alert(3+3) ....we are using nodejs, not browser
+let name = "mehedi"
+let age = 21
+let isLoggedIn = false
+
+// number 
+// bigInt 
+// bollean 
+//string
+// null
+//undefined
+//symbol
+console.log(typeof "Mehedi Hasan Hridoy") 
+console.log(typeof null) // type object
+console.log(typeof undefined); //undefined
+
+
+
---
+++ 01Basics/01variables.js
@@ -0,0 +1,9 @@
+const id = 5550
+let email = "hridoy@google.com"
+var city = "Narayanganj"
+age = 21
+// id = 2 not allowed 
+/*
+Prefer not to use var because of function scope and block scope 
+*/
+console.table([id,email,city,age])

2ab3885efb36e5602df6c35af01098551d18d77a
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-12-14 12:45:03 +0000

    Data Type Summery

--- 01Basics/03conversionOperation.js
+++ 01Basics/03conversionOperation.js
@@ -17,3 +17,31 @@ console.log(boolIsLoggedIn)
 // 0 => false
 // "hridoy" => true
 // "" => false 
+
+//************Operations**************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//Operation
+console.log(2+2)
+console.log(4-3)
+console.log(2*2)
+console.log(4/2)
+console.log(2**3) //two power three
+console.log(4%2)
+
+let str1 = "Mehedi "
+let str2 = "Hasan Hridoy"
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2)   //122
+
+
+
+

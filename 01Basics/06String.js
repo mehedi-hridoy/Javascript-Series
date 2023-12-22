be1ebafc5e64c7befcaa097d88a806280e60ff32
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-12-22 16:00:17 +0000

    Math and Number

---
+++ 01Basics/06String.js
@@ -0,0 +1,11 @@
+const  name = "Mehedi Hasan Hridoy"
+
+const age = 22
+
+console.log(`Hello my name is ${name} and my age is ${age}`)
+
+const myname = new String("Mehedi Hasan Hridoy")
+console.log(myname[0])
+console.log(myname.length)
+console.log(myname.toUpperCase())
+console.log(myname.charAt(3))
---
+++ 01Basics/07Number&Maths.js
@@ -0,0 +1,34 @@
+// const n = 400;
+// console.log(n)
+
+// const n1 = new Number(120)
+// console.log(n1)
+
+// console.log(n1.toString().length)
+// console.log(n1.toFixed(1)) //to print upto n decimal points
+
+// const num = 12347
+// console.log(num.toPrecision(4)) //facing problem with this 
+
+// //++++++ Math +++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.random()) //Gives number in range between 0 to 1
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3,4,5,6))
+// console.log(Math.max(3,4,5,6))
+
+//math random 
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+console.log(Math.floor(Math.random()*10 + 1))
+
+const min = 10 
+const max = 20
+console.log(Math.random()* (max-min+1)+min)
+console.log(Math.floor(Math.random()* (max-min+1)+min))
+
+

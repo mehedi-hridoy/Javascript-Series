10b374731e3744d0dea78e3f91bb96eaa5bf5c50
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-12-22 16:48:44 +0000

    Date and Time

---
+++ 01Basics/08DateAndTime.js
@@ -0,0 +1,24 @@
+//Dates 
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON)
+console.log(myDate.toLocaleDateString())
+
+//Time 
+console.log(Date.now()) //time in miliseconds coundting from 1 Jan 1970
+
+let newDate = new Date()
+console.log(newDate.getDate())
+console.log(newDate.getDay())
+console.log(newDate.getFullYear())
+console.log(newDate.getFullYear())
+console.log(newDate.getHours())
+
+
+
+
+
+

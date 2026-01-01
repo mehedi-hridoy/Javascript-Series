4aa1fde034e5470f2f4c301928970cdd82a300cf
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-01-01 23:37:44 +0600

    for each

---
+++ higherOrderFunctions/01.js
@@ -0,0 +1,8 @@
+// map , filter and reduce are higher order functions in JavaScript that operate on arrays.
+
+// for each loop => calls a function once for each array element
+const a = [1, 2, 3, 4, 5];
+a.forEach((value,index,array) => {
+    console.log(value, index, array);
+})
+

a6e76842137aefdeade617eceb769c6ed43bf57b
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-01-01 23:49:04 +0600

    map & filter

--- higherOrderFunctions/01.js
+++ higherOrderFunctions/01.js
@@ -6,3 +6,15 @@ a.forEach((value,index,array) => {
     console.log(value, index, array);
 })
 
+// map => creates a new array by processing some operation on each array element
+const b = [1, 2, 3, 4, 5];
+b.map((value,index) => {
+    return value * 2;
+})
+
+// filter => filters an array with values that pass a test. creates a new array 
+const c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = c.filter((value) => {
+    return value % 2 === 0;
+})
+console.log(evenNumbers); // [2,4,6,8,10]

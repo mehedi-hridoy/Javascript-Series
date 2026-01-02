071b64dfb7e63a271d759645d12adf0a2cace1f8
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-01-02 23:27:33 +0600

    simple sum

---
+++ higherOrderFunctions/02.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4, 5];;
+
+// sun or max 
+function findSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum; 
+
+}
+
+console.log(findSum(arr)); 
